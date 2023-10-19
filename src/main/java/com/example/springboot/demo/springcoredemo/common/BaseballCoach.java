d5464c69aa634decd88793493f500fa6c2635200
arjunoggu <mallikarjundev2@gmail.com>
2023-10-18 21:09:40 -0400

    using qualifiers to specify the injector

---
+++ src/main/java/com/example/springboot/demo/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.example.springboot.demo.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "spend 30mins";
+    }
+}
---
+++ src/main/java/com/example/springboot/demo/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.example.springboot.demo.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "practice your backhand";
+    }
+}
